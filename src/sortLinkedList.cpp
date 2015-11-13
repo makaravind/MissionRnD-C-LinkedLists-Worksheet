3b9b278b88c1b4b897c19b3d9ca97ec795324aa4
makaravind <makaravind11195@gmail.com>
2015-11-13 14:07:12 +0100

    Submit Commit

--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -12,12 +12,52 @@ NOTES: Without using extra array or linked list.
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
-
+void swap(struct node *a, struct node *b)
+{
+	int temp = a->num;
+	a->num = b->num;
+	b->num = temp;
+}
+int Length(struct node *ptr){
+	struct node *temp = (struct node*)malloc(sizeof(struct node));
+	temp = ptr;
+	int count = 0;
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return count;
+}
 struct node * sortLinkedList(struct node *head) {
-	return NULL;
+	//init checking 
+	if (head == NULL) return NULL;
+
+	int swapped, i;
+	struct node *ptr1;
+	struct node *lptr = NULL;
+
+	do
+	{
+		swapped = 0;
+		ptr1 = head;
+
+		while (ptr1->next != lptr)
+		{
+			if (ptr1->num > ptr1->next->num)
+			{
+				swap(ptr1, ptr1->next);
+				swapped = 1;
+			}
+			ptr1 = ptr1->next;
+		}
+		lptr = ptr1;
+	} while (swapped);
+
+	return head;
 }
